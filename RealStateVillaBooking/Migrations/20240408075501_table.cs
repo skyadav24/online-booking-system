406b20a29ba770ffc7707fcd5b1e05d132bebe72
Santosh Yadav <syadav002@rku.ac.in>
2024-04-08 15:03:04 +0530

    initial commit

---
+++ RealStateVillaBooking/Migrations/20240408075501_table.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealStateVillaBooking.Migrations
+{
+    public partial class table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
